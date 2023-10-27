23f3ac12662cc29e18d699d133d17d6b3dc66420
Bryan Salazar <bryandasalazar@gmail.com>
2023-10-27 12:14:36 -0700

    Version BigCard con estilos

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:provider/provider.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
-
-class MyAppState extends ChangeNotifier{
-  var current = WordPair.random();
-}
-
 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -23,36 +19,77 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-    
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        home: const MyHomePage(),
       ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
+class MyAppState extends ChangeNotifier{
+  var current = WordPair.random();
+
+
+  void getSiguiente() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
 
   @override
-  Widget build (BuildContext context){
-    var appstate = context.watch<MyAppState>();
+  Widget build(BuildContext context){
+    var appState = context.watch<MyAppState>();
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          
-          Text("Emilio"),
-          Text("Gasca"),
-          Text("Mariscal"),
-          Text(appstate.current.asLowerCase),
-  
+          BigCard(idea: appState.current),
+          SizedBox(height: 20,),
+          ElevatedButton(onPressed: () {
+            appState.getSiguiente();
+          },
+          child: Text('Siguiente'),
+          ),
         ],
       ),
     );
   }
 }
 
+
+class BigCard extends StatelessWidget {
+  final WordPair idea;
+  const BigCard({
+    super.key,
+    required this.idea
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    final tema = Theme.of(context);
+    final textStyle = tema.textTheme.displayMedium!.copyWith(
+      color: tema.colorScheme.onPrimary,
+    );
+    return Card(
+    color: tema.colorScheme.primary,
+    child: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Text(
+        
+        idea.asLowerCase,
+        style: textStyle,
+        semanticsLabel: "${idea.first} ${idea.second}"
+),
+    ),
+    );
+  }
+}
