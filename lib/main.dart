a1f911179dadd9583fd8aa77a85821d6a3839e2a
Bryan Salazar <bryandasalazar@gmail.com>
2023-10-27 12:46:39 -0700

    Se añadio un boton

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
           useMaterial3: true,
         ),
         home: const MyHomePage(),
@@ -33,8 +33,21 @@ class MyAppState extends ChangeNotifier{
   var current = WordPair.random();
 
 
+  var favoritos = <WordPair> [];
+
+
   void getSiguiente() {
     current = WordPair.random();
+    notifyListeners();
+  }
+
+
+  void toggleFavorito(){
+    if(favoritos.contains(current)){
+      favoritos.remove(current);
+    } else {
+      favoritos.add(current);
+    }
     notifyListeners();
   }
 }
@@ -47,18 +60,47 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     var appState = context.watch<MyAppState>();
+    var idea = appState.current;
+    IconData icon;
+
+
+     if(appState.favoritos.contains(idea)){
+        icon = Icons.favorite;  
+     } else {
+        icon = Icons.favorite_border_outlined;
+     }
+
+
+
+
+
+
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(idea: appState.current),
-          SizedBox(height: 20,),
-          ElevatedButton(onPressed: () {
-            appState.getSiguiente();
-          },
-          child: Text('Siguiente'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(idea: appState.current),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(onPressed: () {
+                  appState.toggleFavorito();
+                },
+                icon: Icon(icon),
+                label: Text('Me Gusta'),
+                ),
+                SizedBox(width: 10.0),
+                ElevatedButton(onPressed: () {
+                  appState.getSiguiente();
+                },
+                child: Text('Siguiente'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -76,20 +118,21 @@ class BigCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final tema = Theme.of(context);
-    final textStyle = tema.textTheme.displayMedium!.copyWith(
-      color: tema.colorScheme.onPrimary,
-    );
+    final textStyle = tema.textTheme.displayMedium!.copyWith(color:  tema.colorScheme.onPrimary);
+
+
     return Card(
-    color: tema.colorScheme.primary,
-    child: Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Text(
-        
-        idea.asLowerCase,
-        style: textStyle,
-        semanticsLabel: "${idea.first} ${idea.second}"
-),
-    ),
+      color: tema.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          idea.asLowerCase,
+          style: textStyle,
+          semanticsLabel: "${idea.first} ${idea.second}", //se leera cla palabra
+        ),
+
+
+      ),
     );
   }
 }
